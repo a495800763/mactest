42280287444be61ea20913b87e2f7aff98313c65
liumengqi <495800763@qq.com>
2022-09-04 20:10:24 +0800

    optimize  import

--- src/main/java/com/example/springboottest/controller/IndexController.java
+++ src/main/java/com/example/springboottest/controller/IndexController.java
@@ -3,8 +3,6 @@ package com.example.springboottest.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
 @RestController
 public class IndexController {
     @RequestMapping("hello")
